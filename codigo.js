1550fecfb3b28e8e47b7c692f3042e9647411ef7
Gladys Romero <gladysromero@live.com.ar>
2023-08-29 12:14:42 -0300

    con Gif Celebration

--- codigo.js
+++ codigo.js
@@ -1,20 +1,21 @@
-var boton = document.querySelector("#boton"); // Obtiene el elemento del DOM
-var numeroSorteado = 0; // Variable para almacenar el número sorteado
+const boton = document.querySelector("#boton"); // Obtiene el elemento del DOM
+let numeroSorteado = 0; // Variable para almacenar el número sorteado
+const botonX = document.querySelector("button");
+botonX.addEventListener("click", mostrarImagen);
 
 // Evento 'input' para el campo de entrada (#dato)
 const input = document.querySelector("#dato");
-input.addEventListener("input", function () {
+input.addEventListener("click", function () {
   this.value = this.value.replace(/[.,e+\-]/g, ''); // Elimina todo caracter que no sea un nro entero del 1 al 10
 
   let dato = parseInt(this.value);  // Convertir el valor a un número entero usando parseInt
 
-  if (isNaN(dato) || dato < 1 || dato > 10) {  // Verificar si el dato es un número entero válido
+  if (isNaN(dato) || dato == null || dato < 1 || dato > 10) {  // Verificar si el dato es un número entero válido
     this.value = "";  // Si no es un número entero válido, limpiar el campo de entrada
   }
 });
 
 function adivinar() { // Obtiene el n° ingresado y lo compara con el resultado del n° aleatorio
-    debugger;
     let input = document.querySelector("#dato");
     let dato = parseInt(input.value);
     let response = document.querySelector("#respuesta");
@@ -23,17 +24,35 @@ function adivinar() { // Obtiene el n° ingresado y lo compara con el resultado 
 
     if (dato == numeroSorteado) {
         response.innerHTML = "GANASTE!";
+        mostrarImagen();
     } else {
         response.innerHTML = "Perdiste";
     }
 
      // Después de 3 segundos, restablece el título y limpia el campo de entrada
-     setTimeout(() => {
+    setTimeout(() => {
         response.innerHTML = "Adivina el N° que saldrá"; // Restablece el título
+        subtitulo.innerHTML = "(Escriba un número del 1 al 10)";
         input.value = ""; // Limpia el campo de entrada
         let numeros = document.querySelector("#numerosCambiando");
         numeros.innerHTML = ""; //Limpia el nro. sorteado
-    }, 3000); // 3 segundos en milisegundos
+    }, 2000); // 2 segundos en milisegundos
+}
+
+function mostrarImagen() {
+  let divImagen = document.querySelector("#imagen");
+  divImagen.style.display = "block"; //Muestra la imagen
+
+  const botonCerrar = document.querySelector("#cerrarImagen");
+  botonCerrar.addEventListener("click", ocultarImagen);
+}
+
+function ocultarImagen() {
+    let divImagen = document.querySelector("#imagen");
+    divImagen.style.display = "none";
+
+    const botonCerrar = document.querySelector("#cerrarImagen");
+    botonCerrar.removeEventListener("click", ocultarImagen);
 }
 
 function mostrarNumeros() {
@@ -68,88 +87,6 @@ boton.addEventListener("click", () => {
         return;
     }
 
-    numeroSorteado = Math.round(Math.random() * 10); // Genera el número aleatorio sorteado
     mostrarNumeros();
 });
 
-/*
-const boton = document.querySelector("#boton");
-let numeroSorteado = 0;
-
-// Evento 'input' para el campo de entrada (#dato)
-const input = document.querySelector("#dato");
-input.addEventListener("input", function () {
-  // Eliminar cualquier parte decimal y caracteres no permitidos del valor ingresado
-  this.value = this.value.replace(/[.,e+\-]/g, '');
-
-  // Convertir el valor a un número entero usando parseInt
-  let dato = parseInt(this.value);
-
-  // Verificar si el dato es un número entero válido
-  if (isNaN(dato) || dato < 1 || dato > 10) {
-    // Si no es un número entero válido, limpiar el campo de entrada
-    this.value = "";
-  }
-});
-
-function mostrarNumeros() {
-  let contador = 1;
-  let intervalo = setInterval(() => {
-    let numeroAleatorio = Math.round(Math.random() * 10);
-    numerosCambiando.innerHTML = numeroAleatorio;
-    contador++;
-
-    if (contador > 10) {
-      clearInterval(intervalo);
-      numeroSorteado = numeroAleatorio;
-      setTimeout(() => {
-        numerosCambiando.innerHTML = numeroSorteado;
-        adivinar(); // Llama a la función después de mostrar el número sorteado
-      }, 200);
-    }
-  }, 100);
-}
-
-function adivinar() {
-  let input = document.querySelector("#dato");
-  let dato = parseInt(input.value);
-  let response = document.querySelector("#respuesta");
-  let subtitulo = document.querySelector("#subtitulo");
-  subtitulo.innerHTML = "";
-
-  if (dato === numeroSorteado) {
-    response.innerHTML = "GANASTE!";
-    //NUEVO
-    var contenedorGif = document.getElementById('contenedorGif');
-    var gifCelebracion = document.createElement('img');
-    gifCelebracion.src = 'confetti.gif';
-    contenedorGif.appendChild(gifCelebracion);
-    //Nuevo hasta acá
-  } else {
-    response.innerHTML = "Perdiste";
-  }
-
-  setTimeout(() => {
-    response.innerHTML = "Adivina el N° que saldrá";
-    input.value = "";
-    let numeros = document.querySelector("#numerosCambiando");
-    numeros.innerHTML = "";
-    subtitulo.innerHTML = "(Escriba un número del 1 al 10)";
-  }, 2500);
-}
-
-boton.addEventListener("click", () => {
-  let input = document.querySelector("#dato");
-  let dato = parseInt(input.value);
-
-  if (isNaN(dato) || dato < 1 || dato > 10) {
-    alert("Este campo solo acepta números del 1 al 10");
-    input.value = "";
-    return;
-  }
-
-  numeroSorteado = Math.round(Math.random() * 10);
-  mostrarNumeros();
-});
-
-*/
